bb8785f380283cd24b6cd8b428a7f111c0e6fdb7
Chris Evans <chrisevans9629@gmail.com>
2019-11-24 00:00:08 -0600

    adding pen tool for drawing

---
+++ VectorMath/PenTool.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using BasicVector;
+
+namespace VectorMath
+{
+    public abstract class Angle
+    {
+        public double Value { get; set; }
+
+        public static implicit operator double(Angle v)
+        {
+            return v.Value;
+        }
+        
+    }
+    public class DegreeAngle : Angle
+    {
+        public DegreeAngle(double angle)
+        {
+            if(angle > 360 || angle < 0)
+                throw new ArgumentOutOfRangeException(nameof(angle), angle.ToString());
+            Value = angle;
+        }
+
+    }
+    public class RadianAngle : Angle
+    {
+        public RadianAngle(double angle)
+        {
+            if(angle > Math.PI * 2 || angle < 0)
+                throw new ArgumentOutOfRangeException(nameof(angle),angle.ToString());
+            Value = angle;
+        }
+    }
+
+
+    public class PenLine
+    {
+        public double X1 { get; set; }
+        public double X2 { get; set; }
+        public double Y1 { get; set; }
+        public double Y2 { get; set; }
+    }
+
+    public class PenTool
+    {
+
+        public List<PenLine> Lines { get; set; } = new List<PenLine>();
+
+        private Vector _vector = Vector.Zero;
+
+        private RadianAngle radianAngle = new RadianAngle(0);
+        public void MoveTo(double x, double y)
+        {
+            _vector = new Vector(x, y);
+        }
+
+        public RadianAngle ToRadian(DegreeAngle angle)
+        {
+            return new RadianAngle(angle / 57.2957795);
+        }
+
+        public void Rotate(DegreeAngle angle)
+        {
+            var radian = ToRadian(angle);
+            radianAngle = radian; 
+        }
+
+        public void Rotate(RadianAngle angle)
+        {
+            radianAngle = angle;
+        }
+
+        public PenLine Draw(double length)
+        {
+            var drawVector = VectorUtil.Rotate(new Vector(length, 0), radianAngle);
+            var line = new PenLine { X1 = _vector.X, Y1 = _vector.Y, X2 = drawVector.X, Y2 = drawVector.Y};
+            Lines.Add(line);
+            return line;
+        }
+    }
+}
--- VectorMath/Tests.cs
+++ VectorMath/Tests.cs
@@ -9,13 +9,61 @@ using NUnit.Framework;
 namespace VectorMath
 {
     [TestFixture]
+    public class PenToolTests
+    {
+        private PenTool penTool;
+        [SetUp]
+        public void Setup()
+        {
+            penTool = new PenTool();
+        }
+
+        [Test]
+        public void DrawSquare()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                penTool.Draw(10);
+                penTool.Rotate(new DegreeAngle(90));
+            }
+
+            Assert.AreEqual(4, penTool.Lines.Count);
+
+            var last = penTool.Lines.Last();
+            
+            Assert.AreEqual(0, last.X2);
+            Assert.AreEqual(0, last.Y2);
+            Assert.AreEqual(0, last.X1);
+            Assert.AreEqual(10, last.Y1);
+        }
+
+        [Test]
+        public void DrawStraightLine()
+        {
+           var l = penTool.Draw(10);
+
+           Assert.AreEqual(10, l.X2);
+        }
+    }
+
+
+    [TestFixture]
     public class Tests
     {
         [Test]
+        public void ConvertAngle()
+        {
+            var pen = new PenTool();
+            Assert.AreEqual( Math.Round(Math.PI,2), Math.Round(pen.ToRadian(new DegreeAngle(180)).Value,2));
+        }
+
+    
+
+        [Test]
         public void SubtractAndRotate()
         {
-            var vect = new Vector(4,0);
-            var upvect = new Vector(4,4);
+            var vect = new Vector(4, 0);
+            var upvect = new Vector(4, 4);
             var ed = upvect - vect;
             Assert.IsTrue(ed.X == 0 && ed.Y == 4);
             var rot = VectorUtil.Rotate(ed, -Math.PI / 4);
@@ -29,13 +77,13 @@ namespace VectorMath
             var upvect = new Vector(4, 4);
             var ed = upvect - vect;
             Assert.IsTrue(ed.X == 0 && ed.Y == 4);
-            
+
             Assert.IsTrue(ed.Length == 4);
         }
         [SetUp]
         public void Init()
         {
-            
+
         }
     }
 }
